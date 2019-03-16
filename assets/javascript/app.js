1915cc7493ccb053d5c74b91643e36281f06753b
Zach <zachary.fey@gmail.com>
2019-03-16 17:42:07 -0400

    got buttons to populate and gifs to load on click

---
+++ assets/javascript/app.js
@@ -0,0 +1,50 @@
+let buttonList = ['coding', 'wood-working', 'sports', 'traveling', 'painting', 'drawing', 'hiking', 'photography', 'video-games', 'running', 'weightlifting']
+
+let app = {
+    populateButtons: function () {
+        for (let i in buttonList) {
+            var listButton = $('<button>');
+            listButton.attr('id', buttonList[i]);
+            listButton.attr('class', 'gifButton')
+            listButton.text(buttonList[i]);
+            $('#buttonList').prepend(listButton);
+        }
+    },
+    
+    populateGifs: function (buttonID) {
+        console.log('buttonid: ' + buttonID)
+        const queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=nmP5ayP6N65Aw6RBzK4nzC1ZeUkl2wRf&limit=10&q=' + buttonID
+
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(function (response) {
+            console.log(response)
+            const results = response.data
+
+            for (var i = 0; i < results.length; i++) {
+
+                var gifDiv = $('<div>');
+                var p = $('<p>');
+                p.text('Rating: ' + results[i].rating);
+
+                gifImage = $('<img>');
+                gifImage.attr('data-animate', results[i].images.fixed_height.url)
+                gifImage.attr('data-still', results[i].images.fixed_height_still.url)
+                gifImage.attr('src', results[i].images.fixed_height_still.url)
+                gifImage.attr('data-state', 'still')
+
+                gifDiv.append(p);
+                gifDiv.append(gifImage);
+                $('#gifs').prepend(gifDiv);
+            }
+        });
+    }
+}
+
+app.populateButtons();
+
+$('.gifButton').on('click', function(){
+    console.log(this.id);
+    app.populateGifs(this.id);
+})

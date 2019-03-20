4158905f3bf7357334f8ba9129bdd36219276ac3
ZachFey <zachary.fey@gmail.com>
2019-03-19 21:21:37 -0400

    finsihed app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,33 @@
 $(document).ready(function () {
 
 
-    let buttonList = ['coding', 'wood-working', 'sports', 'traveling', 'painting', 'drawing', 'hiking', 'photography', 'video-games', 'running', 'weightlifting']
-
     let app = {
+        hobbyList: ['coding', 'woodworking', 'sports', 'traveling', 'painting', 'drawing', 'hiking', 'photography', 'video games', 'running', 'weightlifting'],
+
         populateButtons: function () {
-            for (let i in buttonList) {
-                var listButton = $('<button>');
-                listButton.attr('id', buttonList[i]);
-                listButton.attr('class', 'gifButton')
-                listButton.text(buttonList[i]);
-                $('#buttonList').prepend(listButton);
+            for (let i in app.hobbyList) {
+                // var listButton = $('<button>');
+                // listButton.addClass('gifButton btn btn-outline-success')
+                // listButton.attr('data-toggle', 'buttons')
+                // listButton.attr('id', app.hobbyList[i]);
+                // listButton.text(app.hobbyList[i]);
+                // $('#buttonList').append(listButton);
+
+                var listLabel = $('<label>');
+                listLabel.addClass('gifButton btn btn-primary')
+                listLabel.attr('id', app.hobbyList[i]);
+                listLabel.text(app.hobbyList[i])
+
+                var listInput = $('<input>');
+                listInput.attr('type', 'radio');
+                listInput.attr('name', 'options');
+                listInput.attr('autocomplete', 'off');
+                // listInput.addClass('gifButton')
+
+                listLabel.append(listInput);
+
+                $('#buttonList').append(listLabel);
+
             }
         },
 
@@ -28,8 +45,9 @@ $(document).ready(function () {
                 for (var i = 0; i < results.length; i++) {
 
                     var gifDiv = $('<div>');
+                    gifDiv.addClass('gifDiv');
                     var p = $('<p>');
-                    p.text('Rating: ' + results[i].rating);
+                    p.text('Rating: ' + results[i].rating.toUpperCase());
 
                     gifImage = $('<img>');
                     gifImage.addClass('gif')
@@ -38,37 +56,45 @@ $(document).ready(function () {
                     gifImage.attr('src', results[i].images.fixed_height_still.url)
                     gifImage.attr('data-state', 'still')
 
-
                     gifDiv.append(p);
-                    gifDiv.append(gifImage);
+                    gifDiv.prepend(gifImage);
                     $('#gifs').prepend(gifDiv);
                 }
             });
+        },
+
+        playPauseGif: function (response) {
+            const clickedGif = $(response.target)
+            const dataState = clickedGif.attr('data-state');
+            console.log('data state: ' + dataState);
+            if (dataState === 'still') {
+                clickedGif.attr('src', clickedGif.attr('data-animate'));
+                clickedGif.attr('data-state', 'animate');
+            } else {
+                clickedGif.attr('src', clickedGif.attr('data-still'));
+                clickedGif.attr('data-state', 'still');
+            }
         }
     }
 
     app.populateButtons();
 
-    $('.gifButton').on('click', function () {
-        console.log(this.id);
+    $(document).on('click', '.gifButton', function () {
+        // console.log(this.id);
+
+        $('#gifs').empty();
         app.populateGifs(this.id);
-
-
     })
 
     $(document).on('click', '.gif', function (response) {
-        const clickedGif = $(response.target)
-        const dataState =  clickedGif.attr('data-state');
-        console.log('data state: ' + dataState);
-        if (dataState === 'still') {
-            clickedGif.attr('src', clickedGif.attr('data-animate'));
-            clickedGif.attr('data-state', 'animate');
-        } else {
-            clickedGif.attr('src', clickedGif.attr('data-still'));
-            clickedGif.attr('data-state', 'still');
-        }
+        app.playPauseGif(response);
     })
 
-
+    $(document).on('click', '#add-hobby', function (event) {
+        event.preventDefault();
+        app.hobbyList.push($('#hobby-input').val().trim())
+        $('#buttonList').empty();
+        app.populateButtons();
+    })
 
 })
